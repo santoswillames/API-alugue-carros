4fc712d255b212e16ad1780eb90f7de75a056db1
Willames Santos <willamessantos@live.com>
2023-02-22 07:47:19 -0300

    create categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World Ignite!" });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
