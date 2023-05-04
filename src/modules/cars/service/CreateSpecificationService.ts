10cad95fbbbfd461acf6d3af82e255edcc985c7c
Willames Santos <willamessantos@live.com>
2023-05-03 23:28:21 -0300

    create services specifications

--- src/modules/cars/service/CreateSpecificationService.ts
+++ src/modules/cars/service/CreateSpecificationService.ts
@@ -1,7 +1,25 @@
-class CreateCategoryService {
-  execute() {
-    console.log("TODO");
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationRepository: ISpecificationsRepository) {}
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification Already exists!");
+    }
+
+    this.specificationRepository.create({
+      name,
+      description,
+    });
   }
 }
 
-export { CreateCategoryService };
+export { CreateSpecificationService };
